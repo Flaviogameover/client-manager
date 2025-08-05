c9aa7aed47941072e5610488654cc8ddfbf236e6
Flávio Lima <flaviogameover@gmail.com>
2025-08-05 14:49:14 -0300

    implement model

--- src/model/Contato.java
+++ src/model/Contato.java
@@ -1,5 +1,49 @@
 package model;
 
 public class Contato {
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
 
+    // Constructor
+    public Contato(int id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/model/GerenciadorContatos.java
+++ src/model/GerenciadorContatos.java
@@ -1,5 +1,51 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenciadorContatos {
+    private List<Contato> contacts;
+    private int nextId;
+
+    // Constructor
+    public GerenciadorContatos() {
+        this.contacts = new ArrayList<Contato>();
+        nextId = 1;
+    }
+
+    // CRUD methods
+    public void addContact(Contato contact) {
+        contact.setId(nextId++);
+        this.contacts.add(contact);
+    }
+
+    public List<Contato> getAllContacts() {
+        return new ArrayList<Contato>(this.contacts); // return copy to avoid external modify
+    }
+
+    public Contato getContact(int id) {
+        for (Contato contact : this.contacts) {
+            if (contact.getId() == id) {
+                return contact;
+            }
+        }
+        System.out.println("Error: Invalid ID");
+        return null;
+    }
+
+    public void refreshContact(Contato refreshedContact) {
+        for (Contato contact : this.contacts) {
+            if (contact.getId() == refreshedContact.getId()) {
+                this.removeContact(contact.getId());
+                this.contacts.add(refreshedContact);
+                return;
+            }
+        }
+    }
+
+    public void removeContact(int id) {
+        Contato contact = this.getContact(id);
+        this.contacts.remove(contact);
+    }
 
 }
